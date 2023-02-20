ff2e07d3a156e4176cb2cb628bc0258459df6545
이지윤 <47352234+Kenshi00@users.noreply.github.com>
2023-02-20 17:22:57 +0900

    [Silver I] Title: 골드바흐의 추측, Time: 2808 ms, Memory: 41192 KB -BaekjoonHub

--- 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
+++ 백준/Silver/6588. 골드바흐의 추측/골드바흐의 추측.py
@@ -7,7 +7,7 @@ if __name__=="__main__":
   # 루트만큼만 확인해도 전체를 확인할 수 있다. - 외우기
   r=1000000
   ch=[0]*(r+1)
-  for i in range(2,1001):
+  for i in range(2,int(math.sqrt(r))):
     if ch[i]==0:
       for j in range(2*i,r+1,i):
         if ch[j]==0:
@@ -18,7 +18,7 @@ if __name__=="__main__":
     cnt=0
     if n==0:
       break
-    for i in range(3,n,2):
+    for i in range(3,n,2): 
       if ch[i]==0 and ch[n-i]==0:
         print(n,'=',i,'+',n-i)
         cnt=1
