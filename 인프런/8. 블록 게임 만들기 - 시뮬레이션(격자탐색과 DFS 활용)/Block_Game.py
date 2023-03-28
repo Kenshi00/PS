909b6b4524b6d747ce2f52a904bc8e957dba8471
dlwldbs9764 <dlwldbs9764@naver.com>
2023-03-28 14:57:35 +0900

    23-03-28 block game 완

--- 인프런/8. 블록 게임 만들기 - 시뮬레이션(격자탐색과 DFS 활용)/Block_Game.py
+++ 인프런/8. 블록 게임 만들기 - 시뮬레이션(격자탐색과 DFS 활용)/Block_Game.py
@@ -29,7 +29,10 @@ def draw_grid(block,grid):
             sc_x=left+(x*22)
             sc_y=top-(y*22)
             block.goto(sc_x,sc_y)
-            block.color(colors[grid[y][x]])
+            if y==15 and grid[y][x]==7:
+                block.color("red")
+            else:
+                block.color(colors[grid[y][x]])
             block.stamp()
 
 def DFS(y, x, grid, color):
@@ -42,6 +45,51 @@ def DFS(y, x, grid, color):
         if 0<yy<24 and 0<xx<13:
             if grid[yy][xx]==color and ch[yy][xx]==0:
                 DFS(yy,xx,grid,color)
+def max_height(grid):
+    for y in range(1,24):
+        for x in range(1,13):
+            if grid[y][x]!=0:
+                return y
+            
+def grid_update(grid, blank):
+    for y, x in blank:
+        grid[y][x]=0
+    height=max_height(grid)
+    for y in range(23, height,-1):
+        for x in range(1,13):
+            if grid[y][x]==0:
+                tmp_y=y
+                while grid[tmp_y-1][x]==0 and tmp_y-1>0:
+                    tmp_y-=1
+                grid[y][x]=grid[tmp_y-1][x]
+                grid[tmp_y-1][x]=0
+
+def continual_remove():
+    global blank, ch
+    while True:
+        flag=1
+        for y in range(23,15,-1):
+            for x in range(1,13):
+                if grid[y][x]!=0:
+                    ch=[[0]*14 for _ in range(25)]
+                    blank=[]
+                    DFS(y, x, grid, grid[y][x])
+                    if len(blank)>=4:
+                        grid_update(grid, blank)
+                        flag=0
+        draw_grid(block,grid)
+        if flag==1:
+            break
+
+def game_over():
+    pen.up()
+    pen.goto(-120,100)
+    pen.write("Game Over",font=("courier",30))
+
+def you_win():
+    pen.up()
+    pen.goto(-100,100)
+    pen.write("You Win",font=("courier",30))
 
 if __name__=="__main__":
 
@@ -70,6 +118,12 @@ if __name__=="__main__":
     grid[brick.y][brick.x]=brick.color
     draw_grid(block, grid)
     
+    pen=t.Turtle()
+    pen.ht()
+    pen.goto(-80,290)
+    pen.color("white")
+    pen.write("Block Game", font=('courier',20,'normal'))
+
     sc.onkeypress(lambda: brick.move_left(grid),"Left")
     sc.onkeypress(lambda: brick.move_right(grid),"Right")
     sc.listen()
@@ -84,7 +138,18 @@ if __name__=="__main__":
             ch=[[0]*14 for _ in range(25)]
             blank=[]
             DFS(brick.y, brick.x, grid, brick.color)
-            print(len(blank))
+            if len(blank)>=4:
+                grid_update(grid, blank)
+                continual_remove()
+
+            height=max_height(grid)
+            if height<=15:
+                game_over()
+                break
+            elif height>=22:
+                draw_grid(block,grid)
+                you_win()
+                break
             brick=Brick()
 
         draw_grid(block,grid)
