3151be98406ec172796750b55fc2509105ade42a
이지윤 <47352234+Kenshi00@users.noreply.github.com>
2026-08-03 15:00:56 +0900

    [level 0] Title: 로그인 성공?, Time: 0.01 ms, Memory: 4.89 MB -BaekjoonHub

---
+++ 프로그래머스/0/120883. 로그인 성공？/로그인 성공？.cpp
@@ -0,0 +1,23 @@
+#include <string>
+#include <vector>
+#include <iostream>
+using namespace std;
+
+string solution(vector<string> id_pw, vector<vector<string>> db) {
+    string answer = "";
+    string id = id_pw[0];
+    string pw = id_pw[1];
+    for(int i = 0; i < db.size(); i++)
+    {
+        if(db[i][0] == id && db[i][1] == pw) 
+        {
+            return "login";   
+        }
+        else if(db[i][0] == id)
+        {
+            answer = "wrong pw";
+        }
+    }
+    if(answer != "wrong pw") answer = "fail";
+    return answer;
+}
