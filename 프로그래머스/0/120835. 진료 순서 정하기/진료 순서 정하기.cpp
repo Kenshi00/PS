27a1756a38d57631e572b88c48e5544cb56c22dd
이지윤 <47352234+Kenshi00@users.noreply.github.com>
2026-08-06 16:57:25 +0900

    [level 0] Title: 진료 순서 정하기, Time: 0.01 ms, Memory: 4.77 MB -BaekjoonHub

---
+++ 프로그래머스/0/120835. 진료 순서 정하기/진료 순서 정하기.cpp
@@ -0,0 +1,16 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+vector<int> solution(vector<int> emergency) {
+    vector<int> answer(emergency.size(),1);
+    for(int i = 0; i < emergency.size(); i++)
+    {
+        for(int j = 0; j < emergency.size(); j++)
+        {
+            if(emergency[i] < emergency[j]) answer[i]++;
+        }
+    }
+    return answer;
+}
