9ccad8c96039e4f5afc34fd466a389a9d1a38e76
이지윤 <47352234+Kenshi00@users.noreply.github.com>
2026-08-11 18:02:57 +0900

    [level 0] Title: 공 던지기, Time: 0.01 ms, Memory: 4.71 MB -BaekjoonHub

---
+++ 프로그래머스/0/120843. 공 던지기/공 던지기.cpp
@@ -0,0 +1,17 @@
+#include <string>
+#include <vector>
+
+using namespace std;
+
+int solution(vector<int> numbers, int k) {
+    int answer = 0;
+    int s = size(numbers);
+    int cnt = 0;
+    while(cnt != k-1)
+    {
+        answer += 2;
+        answer %= s;
+        cnt++;
+    }
+    return numbers[answer];
+}
