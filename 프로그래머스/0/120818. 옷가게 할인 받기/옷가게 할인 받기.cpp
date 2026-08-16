cc0b8030e3b51157ea09316af3eb027b686fe006
이지윤 <47352234+Kenshi00@users.noreply.github.com>
2026-08-16 18:27:02 +0900

    [level 0] Title: 옷가게 할인 받기, Time: 0.01 ms, Memory: 5.01 MB -BaekjoonHub

---
+++ 프로그래머스/0/120818. 옷가게 할인 받기/옷가게 할인 받기.cpp
@@ -0,0 +1,26 @@
+#include <string>
+#include <vector>
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int solution(int price) {
+    int answer = 0;
+    if(100000 <= price && price < 300000)
+    {
+        answer += (price - price * 0.05);
+    }
+    else if(300000 <= price && price < 500000)
+    {
+        answer += (price - price * 0.1);
+    }
+    else if(price >= 500000)
+    {
+        answer += (price - price * 0.2);
+    }
+    else
+    {
+        answer = price;
+    }
+    return answer;
+}
